399e6fb44461930546a628db5f3767bb749a2e15
Saunik Singh <saunik.singh@gmail.com>
2020-07-28 15:13:55 +0530

    List Ui Created

---
+++ app/src/main/java/com/test/dosplash/image/ImageListActivity.kt
@@ -0,0 +1,12 @@
+package com.test.dosplash.image
+
+import android.os.Bundle
+import com.test.dosplash.R
+import com.test.dosplash.base.BaseAcitivity
+
+class ImageListActivity : BaseAcitivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_list)
+    }
+}
--- app/src/main/java/com/test/dosplash/splash/SplashActivity.kt
+++ app/src/main/java/com/test/dosplash/splash/SplashActivity.kt
@@ -1,12 +1,20 @@
 package com.test.dosplash.splash
 
+import android.content.Intent
 import android.os.Bundle
 import com.test.dosplash.R
 import com.test.dosplash.base.BaseAcitivity
+import com.test.dosplash.image.ImageListActivity
+import java.util.*
 
 class SplashActivity : BaseAcitivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                startActivity(Intent(this@SplashActivity, ImageListActivity::class.java))
+            }
+        }, 5000)
     }
 }
